87235ddd8cc0fe532b5c892bda6b19f88e3c2fde
mohan <Mohankrishna.madanapu@arshaa.com>
2022-11-29 16:24:01 +0530

    deploy

--- src/App.js
+++ src/App.js
@@ -3,9 +3,8 @@ import React from 'react'
 const App = () => {
   return (
     <div>
-      <h1>mohan deploy</h1>
-      <h1>mohan deploy</h1>
-      <h1>mohan deploy</h1>
+      <h1>ui deployed</h1>
+      
     </div>
   )
 }

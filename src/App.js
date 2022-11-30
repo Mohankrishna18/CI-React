0df60061b172e3336fe3a73b3b1d6a463935ce0e
mohan <Mohankrishna.madanapu@arshaa.com>
2022-11-30 15:08:31 +0530

    deploy

--- src/App.js
+++ src/App.js
@@ -1,12 +1,12 @@
 import React from 'react';
-//import './App.css';
-import TodoList from './components/TodoList';
+// //import './App.css';
+// import TodoList from './components/TodoList';
 
 function App() {
   return (
     <div  >
       <h2>fucking task done by fucker</h2>
-      <TodoList />
+      {/* <TodoList /> */}
     </div>
   );
 }

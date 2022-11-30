d83424ddf7c8b2739c4762f67861f7448c003ae6
mohan <Mohankrishna.madanapu@arshaa.com>
2022-11-30 15:18:13 +0530

    sfsfs

--- src/App.js
+++ src/App.js
@@ -1,13 +1,13 @@
 import React from 'react';
 // //import './App.css';
-// import TodoList from './components/TodoList';
+ import TodoList from './components/TodoList';
 
 function App() {
   return (
     <div  >
       <h2>fucking task done by fucker</h2>
      
-      {/* <TodoList /> */}
+      <TodoList />
     </div>
   );
 }
